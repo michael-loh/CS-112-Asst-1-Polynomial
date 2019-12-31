e3f142ef1bf1d2988c8fd9fc9918520e5581268f
michael-loh <54383090+michael-loh@users.noreply.github.com>
2019-12-31 16:09:35 -0500

    Update Polynomial.java

--- Polynomial.java
+++ Polynomial.java
@@ -59,10 +59,70 @@ public class Polynomial {
 	 *         is the front of the result polynomial
 	 */
 	public static Node add(Node poly1, Node poly2) {
-		/** COMPLETE THIS METHOD **/
-		// FOLLOWING LINE IS A PLACEHOLDER TO MAKE THIS METHOD COMPILE
-		// CHANGE IT AS NEEDED FOR YOUR IMPLEMENTATION
-		return null;
+		Node sum = null;
+		
+		Node ptr1 = poly1;
+		Node ptr2 = poly2;
+		
+		int size = 0;
+		
+		if(poly1 == null && poly2 == null)
+			return null;
+		else if(poly1 == null)
+			return poly2;
+		else if(poly2 == null)
+			return poly1;
+		
+		for(Node t1 = ptr1; t1.next != null; t1 = t1.next){		//checking size
+			size ++;
+			
+		}
+		
+		int p1d;
+		int p2d;
+		
+		for(int i = 0; i <= size; i++){				//building the linked list
+			if(ptr1 == null && ptr2 == null)
+				break;
+			
+			if(ptr1 == null)
+				p1d = ptr2.term.degree + 1;
+			else
+				p1d = ptr1.term.degree;
+			
+			if(ptr2 == null)
+				p2d = ptr1.term.degree + 1;
+			else
+				p2d = ptr2.term.degree;
+			
+			
+			if(p1d == p2d){
+				if(ptr1.term.coeff + ptr2.term.coeff != 0){
+					sum = new Node(ptr1.term.coeff + ptr2.term.coeff, ptr1.term.degree, sum);
+				}
+				ptr1 = ptr1.next;
+				ptr2 = ptr2.next;
+			}
+			else if(p1d < p2d){
+				sum = new Node(ptr1.term.coeff, ptr1.term.degree, sum);
+				ptr1 = ptr1.next;
+				i--;
+				
+			}
+			else if(p2d < p1d){
+				sum = new Node(ptr2.term.coeff, ptr2.term.degree, sum);
+				ptr2 = ptr2.next;
+				i--;
+			}
+			
+		}
+		
+		Node reverseSum = null;							//reversing linked list to correct order
+		for(Node n1 = sum; n1 != null; n1 = n1.next){
+			reverseSum = new Node(n1.term.coeff, n1.term.degree, reverseSum);
+		}
+		
+		return reverseSum;
 	}
 	
 	/**
@@ -76,10 +136,78 @@ public class Polynomial {
 	 *         is the front of the result polynomial
 	 */
 	public static Node multiply(Node poly1, Node poly2) {
+		Node product = null;
+		Node productSimplified = null;
+		
+		Node ptr1 = poly1;
+		Node ptr2 = poly2;
+		
+		if(poly1 == null || poly2 == null)
+			return null;
+		
+		while(ptr1 != null){
+			
+			ptr2 = poly2;
+			while(ptr2 != null){
+				product = new Node(ptr1.term.coeff*ptr2.term.coeff, ptr1.term.degree + ptr2.term.degree, product);
+				ptr2 = ptr2.next;
+			}
+			ptr1 = ptr1.next;
+		}
+		
+		Node front = product;
+		
+		
+		while(front != null){
+			Node front2 = front.next;
+			Node front3 = front;
+			float coeff = front.term.coeff;
+			while(front2!= null){
+				
+				if(front.term.degree == front2.term.degree){
+					coeff += front2.term.coeff;
+					front3.next = front2.next; 
+					front2 = front2.next;
+				}
+				else{
+					front2 = front2.next;
+					front3 = front3.next;
+				}
+			}
+			productSimplified = new Node(coeff, front.term.degree, productSimplified);
+			front = front.next;
+		}
+		
+		boolean notSorted = true;
+		
+		
+		
+		while(notSorted){
+			for(Node pt = productSimplified; pt != null; pt = pt.next){
+				if(pt.next == null){
+					notSorted = false;
+				}
+				else if(pt.term.degree > pt.next.term.degree){
+					float coeffTemp = pt.term.coeff;
+					int degreeTemp = pt.term.degree;
+					pt.term.coeff = pt.next.term.coeff;
+					pt.term.degree = pt.next.term.degree;
+					pt.next.term.coeff = coeffTemp;
+					pt.next.term.degree = degreeTemp;
+					break;
+				}
+			}
+			
+		}
+		
+		
 		/** COMPLETE THIS METHOD **/
 		// FOLLOWING LINE IS A PLACEHOLDER TO MAKE THIS METHOD COMPILE
 		// CHANGE IT AS NEEDED FOR YOUR IMPLEMENTATION
-		return null;
+		
+		
+		
+		return productSimplified;
 	}
 		
 	/**
@@ -90,10 +218,20 @@ public class Polynomial {
 	 * @return Value of polynomial p at x
 	 */
 	public static float evaluate(Node poly, float x) {
+		float eval = 0;
+		if(poly == null)
+			return 0;
+		
+		Node ptr = poly;
+		
+		while(ptr != null){
+			eval += Math.pow(x, ptr.term.degree) * ptr.term.coeff;
+			ptr = ptr.next;
+		}
 		/** COMPLETE THIS METHOD **/
 		// FOLLOWING LINE IS A PLACEHOLDER TO MAKE THIS METHOD COMPILE
 		// CHANGE IT AS NEEDED FOR YOUR IMPLEMENTATION
-		return 0;
+		return eval;
 	}
 	
 	/**
